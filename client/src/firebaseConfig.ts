b7dca80a5e4a8348b16b97ce966c943b48d371c1
hassanfaleh124 <hassanfaleh2018@gmail.com>
2025-12-28 06:02:17 +0300

    Create firebaseConfig.ts

---
+++ client/src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-RyeNV16aNmWSJIYhQCG_4J_RQDAovds",
+  authDomain: "student-archive-f8689.firebaseapp.com",
+  databaseURL: "https://student-archive-f8689-default-rtdb.firebaseio.com",
+  projectId: "student-archive-f8689",
+  storageBucket: "student-archive-f8689.appspot.com",
+  messagingSenderId: "815104091476",
+  appId: "1:815104091476:web:8e725238d8ebabaf26d4c1",
+  measurementId: "G-X9CCBWXFF0"
+};
+
+// تهيئة Firebase والاتصال بقاعدة البيانات
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
